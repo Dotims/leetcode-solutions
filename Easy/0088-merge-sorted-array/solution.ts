540f770d57b0210ee04ea7f85304d331dfed630c
Dotims <dotims054@gmail.com>
2026-05-26 23:43:48 +0200

    Implement merge function for merging two sorted arrays

--- Easy/0088-merge-sorted-array/solution.ts
+++ Easy/0088-merge-sorted-array/solution.ts
@@ -6,3 +6,25 @@
     Time Complexity: O(m + n)
 */
 
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let r = m + n - 1;
+
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[r] = nums1[p1];
+            p1--;
+        } else {
+            nums1[r] = nums2[p2];
+            p2--;
+        }
+        r--;
+    }
+
+    while (p2 >= 0) {
+        nums1[r] = nums2[p2];
+        p2--;
+        r--;
+    }
+};
